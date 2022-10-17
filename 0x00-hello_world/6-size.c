9f3eeb6d79727ee31a9cdf4785c3633d67f69c6b
Ast-a <bcloverasta905@gmail.com>
2022-10-17 10:13:58 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 		printf("Size of a char: %i byte(s)\n", sizeof(char));
